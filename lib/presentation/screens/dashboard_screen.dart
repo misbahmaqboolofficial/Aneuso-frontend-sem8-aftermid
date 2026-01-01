6828cf466295bd29fe43a86f145e00a38e2838dd
MisbahMaqboolOfficial <misbahmaqboolofficial@gmail.com>
2026-01-01 12:16:27 +0500

    driverid bug fixed.

--- lib/presentation/screens/dashboard_screen.dart
+++ lib/presentation/screens/dashboard_screen.dart
@@ -686,7 +686,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         ),
                         SizedBox(height: 4),
                         Text(
-                          user.fullName + "${user.driverId}",
+                          user.fullName,
                           style: TextStyle(
                             fontSize: 24,
                             fontWeight: FontWeight.w800,
